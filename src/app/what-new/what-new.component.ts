08ca8cf2a8994943a465ddaa5cab19c7227a3698
Heryakos Meles <heryakosmeles@gmail.com>
2024-01-26 03:56:22 -0900

    Fix the Spelling Error

--- src/app/what-new/what-new.component.ts
+++ src/app/what-new/what-new.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-what-new',
   templateUrl: './what-new.component.html',
-  styleUrls: ['./what-new.component.css']
+  styleUrls: ['./what-new.component.css'],
 })
 export class WhatNewComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
